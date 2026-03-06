b0c3d20940244a08387193d36b307e4f4671f072
Stre10k-2k <e43810522@gmail.com>
2026-03-06 19:17:14 +0500

    core views
    adding core views and delete core.models.Cataloge

--- furniture_project/apps/core/models.py
+++ furniture_project/apps/core/models.py
@@ -1,31 +1,26 @@
 from django.db import models
 from django.utils.text import slugify
-
-class Category(models.Model):
+    
+class Product(models.Model):
     name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='images/')
+    rating = models.PositiveSmallIntegerField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     slugify = models.SlugField(unique=True, blank=True)
-
     def save(self, *args, **kwargs):
         if not self.slugify:
             base = slugify(self.name)[:100] or self.name
             candidate_slug = base
             i = 1
-            while Category.objects.filter(slugify=candidate_slug).exists:
+            while Product.objects.filter(slugify=candidate_slug).exists:
                 candidate_slug = f'{base}{i}'
                 i += 1
             self.slugify = candidate_slug
 
         super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
     
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    description = models.TextField(null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='images/')
-    rating = models.PositiveSmallIntegerField()
-    is_active = models.BooleanField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f'self.name'
--- furniture_project/apps/core/views.py
+++ furniture_project/apps/core/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import random
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Product
+from django.db.models import Q
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'core/home.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        qs = Product.objects.filter(
+            Q(is_activate__icontaims = True)
+        )
+
+        ids = list(qs.values_list("id", flat=True)[:200])
+        random_ids = random.sample(ids, min(len(ids), 4)) if ids else []
+        random_obj = list(
+            Product.objects.filter(ids__in=random_ids)
+        )
+
+        random_obj.sort(key=lambda x: random_ids.index(x.id))
+
+        ctx["random_obj"] = random_obj
+
+        return ctx
+
+class ProductViews(ListView):
+    template_name = 'core/products.html'
+    context_object_name = 'product'
+    paginate_by = 12
+
+class ProductDetailView(DetailView):
+    template_name = 'core/detail.html'
+    model = Product
+    slug_field = "slugify"
+    slug_url_kwarg = "slugify"
