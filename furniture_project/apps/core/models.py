a7c7289aa069859a120959c2b0528d4b4b8fd034
Stre10k-2k <e43810522@gmail.com>
2026-03-06 18:56:22 +0500

    models
    added Categories, Products, Order, Feedback

--- furniture_project/apps/core/models.py
+++ furniture_project/apps/core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slugify = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slugify:
+            base = slugify(self.name)[:100] or self.name
+            candidate_slug = base
+            i = 1
+            while Category.objects.filter(slugify=candidate_slug).exists:
+                candidate_slug = f'{base}{i}'
+                i += 1
+            self.slugify = candidate_slug
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='images/')
+    rating = models.PositiveSmallIntegerField()
+    is_active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
--- furniture_project/apps/user_feedback/models.py
+++ furniture_project/apps/user_feedback/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from apps.core.models import Product
 
-# Create your models here.
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=50)
+    adress = models.TextField()
+    message = models.TextField(blank=True)
+
+    class Status(models.TextChoices):
+        PENDING = "Pending", "pending"
+        CONFIREMENT = "Confirment", "confirment"
+        CANCLED = "Cancled", "cancled"
+
+    status = models.CharField(max_length=20, choices=Status.choices, default=Status.PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.product}'
+    
+class Feedback(models.Model):
+    name = models.CharField(max_length=50)
+    role = models.TextField(blank=True)
+    message = models.TextField()
+    rating = models.PositiveSmallIntegerField()
+    is_aprove = models.BooleanField(default=False)
+    submited_at = models.DateTimeField(auto_now_add=True)
